1b74b57cdf6b7e4bc58776985e29d8e5802490c7
thanawat <tha_chookes@hotmail.com>
2016-04-24 13:38:10 +0700

    add

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ app.set('port', (process.env.PORT || 5000))
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '1234') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong validation token')
+})
+
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
