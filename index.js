e3b2bf174988e70e5aff0d2548c2d1d6e241a93c
thanawat <tha_chookes@hotmail.com>
2016-05-27 22:44:37 +0700

    add

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ app.get('/', function (req, res) {
 })
 
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '1234') {
+  if (req.query['hub.verify_token'] === '') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong validation token')
 })
-
 app.post('/webhook/', function (req, res) {
   var messaging_events = req.body.entry[0].messaging
   for (var i = 0; i < messaging_events.length; i++) {
@@ -30,8 +29,7 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200)
 })
-var token = 'EAAWk6sNrfjcBALNrPM8ZBZAadvzid2v0Kv7mZCRvMOsPdKJUcTerfpafbk6ATYazkOJ3Q8WZCbjDyw3b7KlPGrgpjj1clGNNu3QjhIlNhYXLWiZB1gFXbzwOJtpBB5N3xZAmZBJisZBhrPr5lXRaIwrPByBpeLPGR9klzrQJHXJlCAZDZD'
-
+var tokenn = 'EAAWk6sNrfjcBALNrPM8ZBZAadvzid2v0Kv7mZCRvMOsPdKJUcTerfpafbk6ATYazkOJ3Q8WZCbjDyw3b7KlPGrgpjj1clGNNu3QjhIlNhYXLWiZB1gFXbzwOJtpBB5N3xZAmZBJisZBhrPr5lXRaIwrPByBpeLPGR9klzrQJHXJlCAZDZD'
 function sendTextMessage (sender, text) {
   var messageData = {
     text: text
@@ -52,6 +50,8 @@ function sendTextMessage (sender, text) {
     }
   })
 }
+
+
 
 app.set('port', (process.env.PORT || 5000))
 
