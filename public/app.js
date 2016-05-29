58a9b3aad88314452d8f5476c26d04e0f4f5de9c
thanawat <tha_chookes@hotmail.com>
2016-05-29 10:41:37 +0700

    add

--- public/app.js
+++ public/app.js
@@ -2,5 +2,7 @@ angular.module('app', [])
   .controller('TodoListController', function($http) {
     var app = this;
     app.test = "hello"
-
+    app.add = function() {
+      console.log(app.text)
+    }
   });
