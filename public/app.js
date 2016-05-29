fe703156f99d87cbb0834719ab17e3005e54fd35
thanawat <tha_chookes@hotmail.com>
2016-05-29 11:23:41 +0700

    add

--- public/app.js
+++ public/app.js
@@ -13,6 +13,11 @@ angular.module('app', [])
         answer : app.answser
       }
       console.log(data)
+      $http.post('https://sheetsu.com/apis/v1.0/43c348a7757a', data)
+         .then(function success (response) {
+           console.log(response)
+         }, function error (response) {
+       })
     }
     app.re = function (data){
       console.log(data)
