8919f56bf5c90f135ef05d380bd28f4fce3797cd
thanawat <tha_chookes@hotmail.com>
2016-05-29 10:37:49 +0700

    add

--- public/app.js
+++ public/app.js
@@ -4,16 +4,17 @@ angular.module('app', [])
     app.test = "hello tes"
     console.log(app.value)
 
-    app.add = function() {
+    app.click = function() {
         console.log("true")
           $http.get('https://sheetsu.com/apis/v1.0/43c348a7757a'). success(function(response) {
                 console.log(response)
             }).
             error(function(data, status, headers, config) {
               console.log('error')
-      })
+            })
 
-
+      }
+    }
 
 
   });
