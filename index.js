fe3e5202b15bedcae62dedd0dcfabfd5c178d57f
thanawat <tha_chookes@hotmail.com>
2016-05-29 09:59:19 +0700

    add

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express')
-var app = express()
 var bodyParser = require('body-parser')
 var request = require('request')
+var app = express()
+var text
+var token = 'EAAWk6sNrfjcBALNrPM8ZBZAadvzid2v0Kv7mZCRvMOsPdKJUcTerfpafbk6ATYazkOJ3Q8WZCbjDyw3b7KlPGrgpjj1clGNNu3QjhIlNhYXLWiZB1gFXbzwOJtpBB5N3xZAmZBJisZBhrPr5lXRaIwrPByBpeLPGR9klzrQJHXJlCAZDZD'
+var num = 0
+var n = 0
+var count = 0
 
 app.use(bodyParser.json())
 
@@ -15,23 +20,87 @@ app.get('/webhook/', function (req, res) {
   }
   res.send('Error, wrong validation token')
 })
+
 app.post('/webhook/', function (req, res) {
   var messaging_events = req.body.entry[0].messaging
   for (var i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i]
     var sender = event.sender.id
     if (event.message && event.message.text) {
-      var text = event.message.text
-      // Handle a text message from this sender
-      console.log(text)
+      text = event.message.text
 
-      sendTextMessage(sender, 'Text received, echo: ' + text.substr(0, 3))
+      console.log(text)
+      // sendTextMessage(sender, text)
+      var sln = text.length
+      // console.log('length : ' + sln)
+      var getFunc = text.substring(0, 3)
+      console.log('func : ' + getFunc)
+
+      if (getFunc === 'sum') {
+        var gettext = text.substring(4, text.length)
+        console.log('number : ' + gettext)
+        var space = gettext.search(' ')
+        var num1 = parseFloat(gettext.substring(0, space))
+        var num2 = parseFloat(gettext.substring(space, gettext.length))
+        console.log('number1 : ' + num1 + ' number2 : ' + num2)
+        var sum = num1 + num2
+        console.log('sum : ' + sum)
+        sendTextMessage(sender, 'sum : ' + sum)
+      }
+
+      if (getFunc === 'max') {
+        var gettext = text.substring(4, text.length)
+        console.log('number : ' + gettext)
+        var space = gettext.search(' ')
+        var num1 = parseFloat(gettext.substring(0, space))
+        var num2 = parseFloat(gettext.substring(space, gettext.length))
+        console.log('number1 : ' + num1 + ' number2 : ' + num2)
+        if (num1 > num2) {
+          sendTextMessage(sender, 'max : ' + num1)
+        }
+        if (num2 > num1) {
+          sendTextMessage(sender, 'max : ' + num2)
+        }
+      }
+
+      if (getFunc === 'min') {
+        var gettext = text.substring(4, text.length)
+        console.log('number : ' + gettext)
+        var space = gettext.search(' ')
+        var num1 = parseFloat(gettext.substring(0, space))
+        var num2 = parseFloat(gettext.substring(space, gettext.length))
+        console.log('number1 : ' + num1 + ' number2 : ' + num2)
+        if (num1 < num2) {
+          sendTextMessage(sender, 'min : ' + num1)
+        }
+        if (num2 < num1) {
+          sendTextMessage(sender, 'min : ' + num2)
+        }
+      }
+
+      if (getFunc === 'avg') {
+        var gettext = text.substring(4, text.length)
+        console.log('text : ' + gettext)
+        var space = gettext.search(' ')
+        var num1 = parseFloat(gettext.substring(0, space))
+        var gettext2 = gettext.substring(space, text.length - 1)
+        var space2 = gettext2.search(' ')
+        // console.log('sp : ' + space2)
+        if (space2 === 0) {
+          var num2 = parseFloat(gettext2.substring(0, gettext2.search(' ')))
+          console.log('num1 : ' + num1 + ' num2 : ' + num2)
+        }
+      }
     }
   }
   res.sendStatus(200)
 })
 
-var token = 'EAAWk6sNrfjcBALNrPM8ZBZAadvzid2v0Kv7mZCRvMOsPdKJUcTerfpafbk6ATYazkOJ3Q8WZCbjDyw3b7KlPGrgpjj1clGNNu3QjhIlNhYXLWiZB1gFXbzwOJtpBB5N3xZAmZBJisZBhrPr5lXRaIwrPByBpeLPGR9klzrQJHXJlCAZDZD'
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port') + '!')
+})
 
 function sendTextMessage (sender, text) {
   var messageData = {
@@ -43,7 +112,7 @@ function sendTextMessage (sender, text) {
     method: 'POST',
     json: {
       recipient: {id: sender},
-      message: messageData,
+      message: messageData
     }
   }, function (error, response, body) {
     if (error) {
@@ -53,11 +122,3 @@ function sendTextMessage (sender, text) {
     }
   })
 }
-
-
-
-app.set('port', (process.env.PORT || 5000))
-
-app.listen(app.get('port'), function () {
-  console.log('Example app listening on port ' + app.get('port') + '!')
-})
